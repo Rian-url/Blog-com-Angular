0f09aaf56e249c4f4ed240c034db62843c57009f
Dev Rian <rian.jesus5@etec.sp.gov.br>
2024-01-27 20:56:58 -0300

    Inserindo dados nos components de forma dinamica
    Signed-off-by: Dev Rian <rian.jesus5@etec.sp.gov.br>

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photo: string = ""
+  @Input()
+  title: string = ""
+  @Input()
+  link : string = ""
 
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 }
